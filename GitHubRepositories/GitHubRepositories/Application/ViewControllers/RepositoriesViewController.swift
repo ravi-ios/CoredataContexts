e2a6d98b712f8816c7aba6df49ff012aef036361
Ravi <Ravi@apples-macbook-pro.local>
2018-06-17 21:07:30 +0530

    Designed UICollectionVIewCell and populated data on collection view with the help of NSFetchedResultsController.

--- GitHubRepositories/GitHubRepositories/Application/ViewControllers/RepositoriesViewController.swift
+++ GitHubRepositories/GitHubRepositories/Application/ViewControllers/RepositoriesViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+import CoreData
+
 class RepositoriesViewController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
@@ -16,11 +18,22 @@ class RepositoriesViewController: UIViewController {
     
     var query = "tetris"
     
+    lazy var fetchedResultsController: NSFetchedResultsController = { () -> NSFetchedResultsController<NSFetchRequestResult> in
+        
+        let request = NSFetchRequest<NSFetchRequestResult>.init(entityName: Repository.entityName)
+        
+        let descriptors = [NSSortDescriptor.init(key: "date", ascending: true)]
+        request.sortDescriptors = descriptors
+        
+        // init with main context
+        let resultsController = NSFetchedResultsController.init(fetchRequest: request, managedObjectContext: CoreDataManager.shared.managedObjectContext, sectionNameKeyPath: nil, cacheName: nil)
+        
+        return resultsController
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.configureInitialSetup()
-        
-        self.fetchData(pageIndex, query: query)
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,14 +43,71 @@ class RepositoriesViewController: UIViewController {
     fileprivate func configureInitialSetup() {
         self.title = "Repositories"
         self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor: UIColor.white]
+        
+        // Register cell
+        self.collectionView!.register(UINib(nibName: "RepositoryCell", bundle: nil), forCellWithReuseIdentifier: RepositoryCell.cellIdentifier)
+
+        // Assign Delegate and Datasource
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+        
+        
+        
+        // Fetch data from Server
+        self.fetchData(pageIndex, query: query)
     }
     
     fileprivate func fetchData(_ pageIndex: UInt?, query: String?) {
         ServiceManager.fetchServiceData(pageIndex, query: query) { (data, error) in
             if let errorMessage = error?.localizedDescription {
                 UIAlertController.showAlertWith(errorMessage, sender: self)
+            } else {
+                // Fetch data from coredata
+                do {
+                    try self.fetchedResultsController.performFetch()
+                } catch let error {
+                    fatalError(" Error which performFetch \(error.localizedDescription)")
+                }
+                
+                DispatchQueue.main.async(execute: {
+                    self.collectionView.reloadData()
+                })
             }
         }
     }
 }
 
+extension RepositoriesViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.fetchedResultsController.sections?[section].numberOfObjects ?? 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let repository = self.fetchedResultsController.object(at: indexPath) as! Repository
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RepositoryCell.cellIdentifier, for: indexPath) as! RepositoryCell
+        cell.configure(repository)
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 8.0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.frame.size.width - 20
+        let height:CGFloat =  135.0
+        
+        return  CGSize.init(width: width, height: height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
+    }
+}
---
+++ GitHubRepositories/GitHubRepositories/Application/Views/RepositoryCell.swift
@@ -0,0 +1,71 @@
+//
+//  RepositoryCell.swift
+//  GitHubRepositories
+//
+//  Created by Ravi on 17/06/18.
+//  Copyright © 2018 Ravi. All rights reserved.
+//
+
+import UIKit
+
+class RepositoryCell: UICollectionViewCell {
+    
+    static let cellIdentifier = "Cell"
+
+    @IBOutlet weak var backView: UIView!
+    @IBOutlet weak var cardView: UIView!
+    @IBOutlet weak var estimatedDownloadTimeLabel: UILabel!
+    @IBOutlet weak var sizeLabel: UILabel!
+    @IBOutlet weak var ownerLabel: UILabel!
+    @IBOutlet weak var repoTitleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.applyCardEffect()
+    }
+    
+    func configure(_ data: Repository?) {
+        
+        if data?.hasWiki ?? false {
+            self.backView.backgroundColor = UIColor(red:0.03, green:0.47, blue:0.18, alpha:0.4)
+            
+        } else {
+            self.backView.backgroundColor = UIColor(red:0.93, green:0.93, blue:0.93, alpha:0.1)
+        }
+        
+        let name = data?.name ?? ""
+        let ownerName = data?.owner?.name ?? ""
+        
+        repoTitleLabel.text = "Repository: \(name)"
+        ownerLabel.text = "Owner: \(ownerName)"
+        
+        if let size = data?.size {
+            sizeLabel.text = ("Size: \(size) KB")
+        }
+        
+        if let time = data?.estimatedDownloadTime {
+            if time < 120 {
+                estimatedDownloadTimeLabel.text = ("Estimated download time: \(time) sec")
+                
+            } else if time < 3600 {
+                estimatedDownloadTimeLabel.text = ("Estimated download time: \(String(format: "%.0f", Float(time) / 60.0)) min")
+                
+            } else {
+                estimatedDownloadTimeLabel.text = ("Estimated download time: \(String(format: "%.0f", Float(time) / 3600.0)) hr")
+            }
+        }
+    }
+    
+    fileprivate func applyCardEffect() {
+        self.cardView.alpha = 1.0
+        self.cardView.layer.masksToBounds = false
+        self.cardView.layer.cornerRadius = 10.0
+        self.cardView.layer.shadowOffset = CGSize.zero
+        self.cardView.layer.shadowRadius = 2.0
+        self.cardView.layer.shadowColor = UIColor.red.cgColor
+        self.cardView.layer.shadowOpacity = 1.0
+        
+        self.backView.layer.cornerRadius = 10.0
+    }
+
+}
